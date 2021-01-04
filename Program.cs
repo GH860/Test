2796f6ee4b153adbfe22513c66177d61d07b5770
Mars806 <skelete806@gmail.com>
2021-01-04 21:17:16 +0330

    improve code readability and reusability by separate methods Rename variables and classesfirst commit Add project files.

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static readonly String url = "https://www.google.com";
+        static void Main(string[] args)
+        {
+            // Search a text here
+            WriteMessage("Enter a name");
+
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string userInput = Console.ReadLine();
+
+            // initialize a WebDriver instance
+            IWebDriver driver = new ChromeDriver();
+
+            // load google search page
+            driver.Navigate().GoToUrl(url);
+
+            // print title
+            WriteMessage($"Page title: {driver.Title}");
+
+            // enter search word and submit
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys(userInput);
+            element.Submit();
+
+            //create list of data
+            IList<IWebElement> rcElementList = driver.FindElements(By.ClassName("rc"));
+            List<SearchResultModel> dataModels = GetSearchResultModelList(rcElementList);
+
+            WriteMessage($"done:  {dataModels.Count}");
+
+        }
+
+        //extract link and title from element
+        static SearchResultModel ExtractSearchResultModel(IWebElement webElement)
+        {
+            SearchResultModel searchResultModel = new SearchResultModel();
+            try
+            {
+                IWebElement web = webElement.FindElement(By.TagName("a"));
+
+                searchResultModel.Link = web.GetAttribute("href");
+                searchResultModel.Title = webElement.Text;
+            }
+            catch(NotFoundException ex)
+            {
+                Debug.WriteLine(ex.ToString());
+
+                searchResultModel.Link = "";
+                searchResultModel.Title = "";
+            }
+
+            return searchResultModel;
+        }
+
+        static List<SearchResultModel> GetSearchResultModelList(IList<IWebElement> webElementList)
+        {
+            List<SearchResultModel> searchResultModelList = new List<SearchResultModel>();
+
+            foreach (IWebElement webElement in webElementList)
+            {
+                SearchResultModel searchResultModel = ExtractSearchResultModel(webElement);
+                searchResultModelList.Add(searchResultModel);
+
+                WriteMessage($"dataModel : \n link : { searchResultModel.Link} \n title :  { searchResultModel.Title}");
+                WriteMessage("\n --------------------------------------------------------------------");
+            }
+
+            return searchResultModelList;
+        }
+
+        static void WriteMessage(String value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ SearchResultModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class SearchResultModel
+    {
+        public String Link { get; set; }
+        public String Title { get; set; }
+    }
+}
