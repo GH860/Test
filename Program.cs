875f11bffbe644435903267f351c8ee24bc03c06
Mars806 <skelete806@gmail.com>
2021-01-05 16:07:17 +0330

    I can not push this commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Threading;
 
 namespace ConsoleApp1
 {
@@ -39,28 +42,7 @@ namespace ConsoleApp1
 
         }
 
-        //extract link and title from element
-        static SearchResultModel ExtractSearchResultModel(IWebElement webElement)
-        {
-            SearchResultModel searchResultModel = new SearchResultModel();
-            try
-            {
-                IWebElement web = webElement.FindElement(By.TagName("a"));
-
-                searchResultModel.Link = web.GetAttribute("href");
-                searchResultModel.Title = webElement.Text;
-            }
-            catch(NotFoundException ex)
-            {
-                Debug.WriteLine(ex.ToString());
-
-                searchResultModel.Link = "";
-                searchResultModel.Title = "";
-            }
-
-            return searchResultModel;
-        }
-
+        //get list of results model
         static List<SearchResultModel> GetSearchResultModelList(IList<IWebElement> webElementList)
         {
             List<SearchResultModel> searchResultModelList = new List<SearchResultModel>();
@@ -77,6 +59,30 @@ namespace ConsoleApp1
             return searchResultModelList;
         }
 
+
+        //extract link and title from element
+        static SearchResultModel ExtractSearchResultModel(IWebElement webElement)
+        {
+            SearchResultModel searchResultModel = new SearchResultModel();
+            try
+            {
+                IWebElement web = webElement.FindElement(By.TagName("a"));
+
+                searchResultModel.Link = web.GetAttribute("href");
+                searchResultModel.Title = webElement.Text;
+            }
+            catch (NotFoundException ex)
+            {
+                Debug.WriteLine(ex.ToString());
+
+                searchResultModel.Link = "";
+                searchResultModel.Title = "";
+            }
+
+            return searchResultModel;
+        }
+
+        //print String
         static void WriteMessage(String value)
         {
             Console.WriteLine(value);
